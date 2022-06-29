804fe4fb57b21a5025daa9e4c7b59d9be7d19e93
Benjamin <benj.james.carter@gmail.com>
2022-06-28 22:21:43 -0700

    added fretboard class, with note postions and tests

--- tests/test_fretboard.py
+++ tests/test_fretboard.py
@@ -4,8 +4,32 @@ def test_fretboard_exists():
     assert Fretboard
     
     
-def test_highE_list():
+def test_EString():
     frets = Fretboard()
-    expected = ['E','F', "F#", 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']
+    expected = ['E', 'F', "F#", 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']
     assert frets.EString == expected
+    
+
+def test_BString():
+    frets = Fretboard()
+    expected = ['B', 'C', 'C#', 'D', 'D#', 'E', 'F', "F#", 'G', 'G#', 'A', 'A#', 'B'] 
+    assert frets.BString == expected
+    
+    
+def test_GString():
+    frets = Fretboard()
+    expected = ['G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E','F', "F#", 'G']
+    assert frets.GString == expected
+    
+    
+def test_DString():
+    frets = Fretboard()
+    expected = ['D', 'D#', 'E', 'F', "F#", 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D']
+    assert frets.DString == expected
+    
+    
+def test_AString():
+    frets = Fretboard()
+    expected = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', "F#", 'G', 'G#', 'A' ]
+    assert frets.AString == expected
     
